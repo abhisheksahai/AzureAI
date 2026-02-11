589c24c79891509ecc093c6ef08eabc91eb7e3f6
absahai <abhishek.sahai@outlook.in>
2026-02-11 11:03:06 +0530

    document analysis

--- DocumentAnalysis/Pages/Index.cshtml.cs
+++ DocumentAnalysis/Pages/Index.cshtml.cs
@@ -12,8 +12,18 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
+    [BindProperty]
+    public IFormFile Document { get; set; }
+
     public void OnGet()
     {
 
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        stringe endpoint="https://azure-ai-documentintelligence.cognitiveservices.azure.com/";
+        string key="EuirCu05iY4oCfNb0Jq6vpJQEWJJhR8OPnFiatg6fXMTsBQlLGSqJQQJ99CBACYeBjFXJ3w3AAALACOG01ed";
+        return RedirectToPage("Success");
     }
 }
