add1144af3d091a259cb22471ed8e59ecdeec6d1
absahai <abhishek.sahai@outlook.in>
2026-05-05 15:04:34 +0530

    create test project

--- DSA/Tutort.DSA.Test/20260505/UnitTestAssignment.cs
+++ DSA/Tutort.DSA.Test/20260505/UnitTestAssignment.cs
@@ -2,15 +2,24 @@
 {
 	public class UnitTestAssignment
 	{
+		private Assignment _assignment;
+
 		[SetUp]
 		public void Setup()
 		{
+			_assignment = new Assignment();
 		}
 
-		[Test]
-		public void Test1()
+		[TestCase(1, 2, 3, ExpectedResult = 1)]
+		[TestCase(3, 2, 1, ExpectedResult = 1)]
+		[TestCase(2, 1, 3, ExpectedResult = 1)]
+		[TestCase(5, 5, 5, ExpectedResult = 5)]
+		[TestCase(-1, -2, -3, ExpectedResult = -3)]
+		[TestCase(0, -1, 1, ExpectedResult = -1)]
+		[TestCase(int.MinValue, 0, int.MaxValue, ExpectedResult = int.MinValue)]
+		public int MinimumThreeNumbers_ReturnsMinimum(int x, int y, int z)
 		{
-			Assert.Pass();
+			return _assignment.MinimumThreeNumbers(x, y, z);
 		}
 	}
 }
--- DSA/Tutort.DSA/20260505/Assignment.cs
+++ DSA/Tutort.DSA/20260505/Assignment.cs
@@ -2,6 +2,18 @@
 {
 	public class Assignment
 	{
-
+		public int MinimumThreeNumbers(int x, int y, int z)
+		{
+			int result = z;
+			if (x < y && x < z)
+			{
+				result = x;
+			}
+			else if (y < x && y < z)
+			{
+				result = y;
+			}
+			return result;
+		}
 	}
 }
