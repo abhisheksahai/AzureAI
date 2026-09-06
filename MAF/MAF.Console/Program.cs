a22ea5af32f127b1ab7a282f5934f5134d67d7c4
absahai <abhishek.sahai@outlook.in>
2026-09-06 08:54:43 +0530

    Initial changes for Microsoft Agent Framework

---
+++ MAF/MAF.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+using OpenAI.Chat;
+
+var endPoint = "https://azureopenai-maf-resource.services.ai.azure.com";
+var model = "gpt-5.5";
+
+AIAgent agent = new AzureOpenAIClient(new Uri(endPoint), new AzureCliCredential()).GetChatClient(model).AsAIAgent(instructions: "you are a friendly assistant. keep your answers brief");
+var response = await agent.RunAsync("Tell me about Royal Enfield Classic 350 - Stelth Black");
+Console.WriteLine(response);
